5a7db88a4d01d4bf11ba8366f11f4c20470f8876
EdisonOspina16 <edison100ospina@gmail.com>
2023-08-08 20:11:35 -0500

    ...

--- EJERCICIOS/ejercicio_6.py
+++ EJERCICIOS/ejercicio_6.py
@@ -1,27 +1,15 @@
-class Rectangulo:
-    def __init__(self, p1, p2):
-        self.p1 = p1
-        self.p2 = p2
+class CuentaBancaria:
 
-    def perimetro(self):
-        longitud = abs(self.p2[0] - self.p1[0])
-        ancho = abs(self.p2[1] - self.p1[1])
-        return (longitud + ancho) * 2
+    def __init__(self, numero_cuenta, propietarios, balance):
 
-    def area(self):
-        longitud = abs(self.p2[0] - self.p1[0])
-        ancho = abs(self.p2[1] - self.p1[1])
-        return longitud * ancho
-
-    def es_cuadrado(self):
-        longitud = abs(self.p2[0] - self.p1[0])
-        ancho = abs(self.p2[1] - self.p1[1])
-        return longitud == ancho
+        self.numero_cuenta = numero_cuenta
+        self.propietarios = propietarios
+        self.balance = balance
 
 
-r = Rectangulo((0, 0), (4, 3))
-print(r.perimetro())
-print(r.area())
-print(r.es_cuadrado())
+    def mostrar_datos(self):
+        print(f"Número de cuenta: {self.numero_cuenta}")
+        print(f"Propietarios: {self.propietarios}")
+        print(f"Balance: {self.balance}")
 
 
