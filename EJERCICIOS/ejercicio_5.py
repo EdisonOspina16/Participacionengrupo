2c3f095a6b1bf3fc744ba5075870ec471424b299
EdisonOspina16 <edison100ospina@gmail.com>
2023-08-08 20:26:20 -0500

    arreglos en algunos ejercicios

--- EJERCICIOS/ejercicio_5.py
+++ EJERCICIOS/ejercicio_5.py
@@ -1,3 +1,9 @@
+#Cree una clase Circulo que tenga las propiedades centro
+# y radio, las cuales se inicializan con parámetros en el constructor.
+# Defina métodos en la clase para calcular el área, el perímetro e indicar
+# si un punto pertenece al círculo o no.
+
+
 import math
 
 
--- EJERCICIOS/ejercicio_7.py
+++ EJERCICIOS/ejercicio_7.py
@@ -1,3 +1,8 @@
+#Cree una clase CuentaBancaria que contenga los siguientes atributos:
+# numero_cuenta, propietarios y balance. Los tres atributos se deben
+# inicializar en el constructor con valores recibidos como parámetros.
+
+
 class CuentaBancaria:
 
     def __init__(self, numero_cuenta, propietarios, balance):
